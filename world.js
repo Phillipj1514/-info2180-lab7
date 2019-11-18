7f02c36dcfd77ae44babc1382c2fa9170f222d68
phillipj1514 <phillipjunior95@gmail.com>
2019-11-18 18:25:38 -0500

    searching for a country using phraes

---
+++ world.js
@@ -0,0 +1,22 @@
+let country = null;
+let results = null;
+document.addEventListener("DOMContentLoaded", initiate);
+function initiate(){
+    country = document.getElementById("country");
+    results = document.getElementById("result");
+    document.getElementById("lookup").addEventListener("click", getData);
+}
+function getData(){
+    const superhero = new XMLHttpRequest();
+    superhero.onreadystatechange = function (){
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            results.innerHTML = this.responseText;
+
+        }
+    };
+
+    superhero.open("GET", "world.php?country="+country.value,true);
+    superhero.send();
+    country.value = "";
+    
+}
--- world.php
+++ world.php
@@ -1,11 +1,14 @@
 <?php
 $host = getenv('IP');
+//$host = 'localhost';
 $username = 'lab7_user';
-$password = '';
+$password = 'password';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+
+$country =$_REQUEST["country"];
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
