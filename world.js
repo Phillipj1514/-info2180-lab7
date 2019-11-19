4a0ae23c82da72207fdfe9b32d4a87ac2cbaf197
phillipj1514 <phillipjunior95@gmail.com>
2019-11-19 02:01:42 -0500

    city search

--- world.js
+++ world.js
@@ -5,18 +5,27 @@ function initiate(){
     country = document.getElementById("country");
     results = document.getElementById("result");
     document.getElementById("lookup").addEventListener("click", getCountry);
+    document.getElementById("lookup_c").addEventListener("click", getCities);
 }
 function getCountry(){
     const superhero = new XMLHttpRequest();
     superhero.onreadystatechange = function (){
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             results.innerHTML = this.responseText;
-
         }
     };
-
     superhero.open("GET", "world.php?country="+country.value,true);
     superhero.send();
-    country.value = "";
-    
+    country.value = ""; 
+}
+function getCities(){
+    const superhero = new XMLHttpRequest();
+    superhero.onreadystatechange = function (){
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            results.innerHTML = this.responseText;
+        }
+    };
+    superhero.open("GET", "world.php?country="+country.value+"&context=cities",true);
+    superhero.send();
+    country.value = "";   
 }
--- world.php
+++ world.php
@@ -7,30 +7,43 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-$country =$_REQUEST["country"];
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$country_list = "<table>
+$country = $_REQUEST['country'];
+if(count($_REQUEST) == 2){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM countries INNER JOIN cities ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%';"); 
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $country_list = "<table>
                 <tr>
-                  <th>Country Name</th>
-                  <th>Continent</th>
-                  <th>Independence Year</th>
-                  <th>Head of State</th>
+                  <th>City Name</th>
+                  <th>District</th>
+                  <th>Population</th>
                 </tr>
-                </table>";
-foreach ($results as $row){
-  $country_list = $country_list.'
-  <tr>
-    <td>'.$row['name'].'</td>
-    <td>'.$row['continent'].'</td>
-    <td>'.$row['independence_year'].'</td>
-    <td>'.$row['head_of_state'].'</td>
-  </tr>';
-}
-$country_list = $country_list.'</table>';
+                ";
+  foreach ($results as $row){
+    $country_list = $country_list.'
+    <tr>
+      <td>'.$row['name'].'</td>
+      <td>'.$row['district'].'</td>
+      <td>'.$row['population'].'</td>
+    </tr>';
+  }
+  //print_r($results);
 
+}else{
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($results as $row){
+    $country_list = $country_list.'
+    <tr>
+      <td>'.$row['name'].'</td>
+      <td>'.$row['continent'].'</td>
+      <td>'.$row['independence_year'].'</td>
+      <td>'.$row['head_of_state'].'</td>
+    </tr>';
+  }
+}
+
+$country_list = $country_list.'</table>';
 echo $country_list
 
 ?>
